9bb4c2db8d4ce5a134ebedcf28e3a96965e7376f
Rafael Santos <rafael.santos@acal.local>
2022-03-09 17:48:29 -0300

    first commit

--- index.js
+++ index.js
@@ -1,26 +1,181 @@
 const readline = require('readline')
+const os = require('os')
 const fs = require('fs')
 const path = require("path")
 const { exit } = require('process')
 // const cam = path.join(__dirname,"/data/fluigs.json")
 const cam2 = "C:\\Users\\rafael.santos\\Documents\\projects\\Desktop\\NodeJS\\NoteFluigCLI\\data\\fluigs.json"
 
-// console.log(cam)
+const pathOS = {
+  path: {
+    linux: '/tmp',
+    win: 'C:\\',
+  },
+  folder: 'fluigs',
+  file: 'fluigs.json'
+}
+
+var flag = true
+var status = ""
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 })
 
+// Verify platform OS:
+// Possible values are 'aix', 'darwin', 'freebsd', 'linux', 'openbsd', 'sunos', and 'win32'
+if (os.platform() == 'win32') {
+  // Verify if exist path:
+  if (fs.existsSync(path.join(pathOS.path.win, pathOS.folder))) {
+    if (fs.readdirSync(path.join(pathOS.path.win, pathOS.folder)).length >= 1) {
+      console.log(`Diretório com ${fs.readdirSync(path.join(pathOS.path.win, pathOS.folder)).length} arquivos!`)
+      rl.question('Numero do fluig? ', function(num_fluig){
+        rl.question('Descrição do fluig? ', function(desc_fluig){
+          
+          const content = {
+            num_fluig: num_fluig,
+            desc_fluig: desc_fluig,
+            status: ""
+          }
+    
+          rl.question(`Status da resolução \n
+            1 - Assumido
+            2 - Em aguardo
+            3 - Finalizado
+            \n>> `, function(status) {
+    
+              console.log(`Status do fluig atual?
+              Fluig: ${num_fluig}
+              Descrição: ${desc_fluig}
+              Status: ${status}`)
+              
+              if(status == 3) {
+    
+              status = "Finalizado"
+              readWrite(content, status)
+            } else if(status == 2) {
+              
+              status = "Em aguardo"
+              readWrite(content, status)
+            } else if(status == 1) {
+              
+              status = "Assumido"
+              readWrite(content, status)
+            } else {
+              manFluigs()
+            }
+          })
+        })
+      })
+      
+    } else {
+      console.log(`Diretório com ${fs.readdirSync(path.join(pathOS.path.win, pathOS.folder)).length} arquivos!`)
+      
+      rl.question('Numero do fluig? ', function(num_fluig){
+        rl.question('Descrição do fluig? ', function(desc_fluig){
+          
+          const content = {
+            num_fluig: num_fluig,
+            desc_fluig: desc_fluig,
+            status: ""
+          }
+    
+          rl.question(`Status da resolução \n
+            1 - Assumido
+            2 - Em aguardo
+            3 - Finalizado
+            \n>> `, function(status) {
+    
+              console.log(`Status do fluig atual?
+              Fluig: ${num_fluig}
+              Descrição: ${desc_fluig}
+              Status: ${status}`)
+              
+              if(status == 3) {
+    
+              status = "Finalizado"
+              readWrite(content, status)
+            } else if(status == 2) {
+              
+              status = "Em aguardo"
+              readWrite(content, status)
+            } else if(status == 1) {
+              
+              status = "Assumido"
+              readWrite(content, status)
+            } else {
+              manFluigs()
+            }
+          })
+        })
+      })
+
+
+    }
+
+  } else {
+    console.log(`Directory ${pathOS.path.win+pathOS.folder} Not exist!!!`)
+    fs.mkdir(path.join(pathOS.path.win, pathOS.folder), { recursive: true }, (err) => {
+      if(err) return console.log(err)
+      
+      console.log(`Creating ${pathOS.path.win+pathOS.folder} ...`)
+      console.log(`Directory ${pathOS.path.win+pathOS.folder} criado com sucesso!`)
+
+      rl.question('Numero do fluig? ', function(num_fluig){
+        rl.question('Descrição do fluig? ', function(desc_fluig){
+          
+          const content = {
+            num_fluig: num_fluig,
+            desc_fluig: desc_fluig,
+            status: ""
+          }
+    
+          rl.question(`Status da resolução \n
+            1 - Assumido
+            2 - Em aguardo
+            3 - Finalizado
+            \n>> `, function(status) {
+    
+              console.log(`Status do fluig atual?
+              Fluig: ${num_fluig}
+              Descrição: ${desc_fluig}
+              Status: ${status}`)
+              
+              if(status == 3) {
+    
+              status = "Finalizado"
+              readWrite(content, status)
+            } else if(status == 2) {
+              
+              status = "Em aguardo"
+              readWrite(content, status)
+            } else if(status == 1) {
+              
+              status = "Assumido"
+              readWrite(content, status)
+            } else {
+              manFluigs()
+            }
+          })
+        })
+      })
+
+
+    })
+  }
+} 
+
+
 function readWrite(content, status) {
-  fs.readFile(cam2, (err, data) => {
+  fs.readFile(path.join(pathOS.path.win, pathOS.file), (err, data) => {
     if(err) throw err
 
     const arq = JSON.parse(data)
     content.status = status
     arq.push(content)
-
-    fs.writeFile(cam2, JSON.stringify(arq, null, 2), (err) => {
+    
+    fs.writeFile(path.join(pathOS.path.win, pathOS.file), JSON.stringify(arq, null, 2), (err) => {
       if(err) throw err
       console.log('The file has been saved!')
       // rl.close()
@@ -29,14 +184,10 @@ function readWrite(content, status) {
   })
 }
 
-
-var flag = true
-var status = ""
-
 var manFluigs = function(){
-  rl.question('Numero do fluig? ', function(num_fluig){
+  return  rl.question('Numero do fluig? ', function(num_fluig){
     rl.question('Descrição do fluig? ', function(desc_fluig){
-
+      
       const content = {
         num_fluig: num_fluig,
         desc_fluig: desc_fluig,
@@ -49,12 +200,12 @@ var manFluigs = function(){
         3 - Finalizado
         \n>> `, function(status) {
 
-        console.log(`Status do fluig atual:
+          console.log(`Status do fluig atual?
           Fluig: ${num_fluig}
           Descrição: ${desc_fluig}
           Status: ${status}`)
-
-        if(status == 3) {
+          
+          if(status == 3) {
 
           status = "Finalizado"
           readWrite(content, status)
@@ -63,7 +214,7 @@ var manFluigs = function(){
           status = "Em aguardo"
           readWrite(content, status)
         } else if(status == 1) {
-
+          
           status = "Assumido"
           readWrite(content, status)
         } else {
@@ -72,14 +223,87 @@ var manFluigs = function(){
       })
     })
   })
+}
+
+
+
+
+
+
+
+
+
+
+// Working on:
+
+// function readWrite(content, status) {
+//   fs.readFile(cam2, (err, data) => {
+//     if(err) throw err
+
+//     const arq = JSON.parse(data)
+//     content.status = status
+//     arq.push(content)
+    
+//     fs.writeFile(cam2, JSON.stringify(arq, null, 2), (err) => {
+//       if(err) throw err
+//       console.log('The file has been saved!')
+//       // rl.close()
+//       manFluigs()
+//     })
+//   })
+// }
+
+
+
+// var manFluigs = function(){
+//   rl.question('Numero do fluig? ', function(num_fluig){
+//     rl.question('Descrição do fluig? ', function(desc_fluig){
+      
+//       const content = {
+//         num_fluig: num_fluig,
+//         desc_fluig: desc_fluig,
+//         status: ""
+//       }
+
+//       rl.question(`Status da resolução \n
+//         1 - Assumido
+//         2 - Em aguardo
+//         3 - Finalizado
+//         \n>> `, function(status) {
+
+//           console.log(`Status do fluig atual?
+//           Fluig: ${num_fluig}
+//           Descrição: ${desc_fluig}
+//           Status: ${status}`)
+          
+//           if(status == 3) {
+
+//           status = "Finalizado"
+//           readWrite(content, status)
+//         } else if(status == 2) {
+          
+//           status = "Em aguardo"
+//           readWrite(content, status)
+//         } else if(status == 1) {
+          
+//           status = "Assumido"
+//           readWrite(content, status)
+//         } else {
+//           manFluigs()
+//         }
+//       })
+//     })
+//   })
 
   // rl.on('close', () => {
   //   console.log("\nApp finalizado!")
   //   process.exit(0)
   // })
-}
+// }
 
-manFluigs()
+// manFluigs()
+
+
 
 // rl.question(`######### Gerenciamento de resolução de chamados de fluigs diários ##########
 //   Digite a opção abaixo para executar:
@@ -87,11 +311,11 @@ manFluigs()
 //   2 - Listar todos os atendimentos:
 //   0 - Sair do sistem
 // `, function(op) {
-
-//   if(op == 1) {
-//     manFluigs()
+  
+  //   if(op == 1) {
+    //     manFluigs()
 //   } else if (op == 0) {
-//     flag = false
+  //     flag = false
 //     console.log("Aplicação finalizada!")
 //     exit(0)
 //   } else {
@@ -99,3 +323,20 @@ manFluigs()
 //     return
 //   }
 // })
+
+// else if (os.platform() == 'linux') {
+//   // Verify if exist path:
+//   if (fs.existsSync(path.join(pathOS.path.linux, pathOS.folder))) {
+//     return console.log('The path exists!')
+//   } else {
+//     console.log('Path Not exist!')
+//     console.log('Creating Folder in path...')
+//     fs.mkdir(path.join(pathOS.path.linux, pathOS.folder), { recursive: true }, (err) => {
+//       if(err) return console.log(err)
+
+//       return console.log(`Diretório ${pathOS.path.win+pathOS.folder} criado com sucesso!`)
+//     })
+//   }
+// } else {
+//   console.log('Platform not found!')
+// }
